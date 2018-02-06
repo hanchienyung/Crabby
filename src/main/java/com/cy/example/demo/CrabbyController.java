66b244737f7e77a955d8a31d23011d53e590202b
hanchienyung <hanchienyung@yahoo.com>
2018-02-05 20:18:30 -0500

    Added crabbypage.html

--- src/main/java/com/cy/example/demo/CrabbyController.java
+++ src/main/java/com/cy/example/demo/CrabbyController.java
@@ -1,7 +1,12 @@
 package com.cy.example.demo;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class CrabbyController {
-    @RequestMapping("/"
-    public String list(){
-        return "list"
+    @RequestMapping("/")
+    public String list() {
+        return "crabbypage";
+    }
 }
